12d44f7e9af10bdae64958879f2a6dc659844e32
Rabia Syed <160381125+aqsarabia@users.noreply.github.com>
2024-03-07 15:17:09 +0500

    assignment 10,11,12
    i am the student of miss hina naseer..this is my coding..

---
+++ main.js
@@ -0,0 +1,21 @@
+//Q10:
+var whitespace = "\n\tAqsa Rabia\t\n";
+console.log(whitespace);
+var withoutwhitespace = whitespace.trim();
+console.log(withoutwhitespace);
+// Q11:
+var friendsname = ["Aqsa", "Areeba", "Alia", "Fatima", "Sehar"];
+console.log(friendsname[0]);
+console.log(friendsname[1]);
+console.log(friendsname[2]);
+console.log(friendsname[3]);
+console.log(friendsname[4]);
+// Q12:
+var guys = ["Aqsa", "Areeba", "Alia", "Fatima", "Sehar"];
+var message = ["How was the day,welcome to my Website"];
+var pronouns = ("HEY");
+console.log(pronouns, guys[0], message);
+console.log(pronouns, guys[1], message);
+console.log(pronouns, guys[2], message);
+console.log(pronouns, guys[3], message);
+console.log(pronouns, guys[4], message);
